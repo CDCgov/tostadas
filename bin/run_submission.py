50c7a230d0c61f79ef992aaf73a36ae71348ca1c
Cole tindall <113549262+CTindall-1@users.noreply.github.com>
2023-01-31 15:54:27 -0500

    Update run_submission.py

--- bin/run_submission.py
+++ bin/run_submission.py
@@ -51,7 +51,7 @@ class SubmitToDatabase:
             #work_dir= os.mkdir("/data/nf" )
             self.parameters['nf_output_dir'] = f"{self.parameters['nf_output_dir']}"
             if self.parameters['entry_flag'].lower() != 'true':
-                out_dir = os.makedirs(out_dir)
+                out_dir = os.mkdir(out_dir)
                 self.parameters['validated_meta_path'] = f"{out_dir/self.parameters['validated_meta_path']}"
                 self.parameters['lifted_fasta_path'] = f"{out_dir/self.parameters['lifted_fasta_path']}"
                 self.parameters['lifted_gff_path'] = f"{out_dir/self.parameters['lifted_gff_path']}"
