ae71f3dd660d1b8beba7d8a82c55b98866ac31d8
RamiyapriyaS <ramsivakumar@deloitte.com>
2025-08-29 11:01:00 -0400

    modified nf-tests

--- .nf-test/tests/23359d2df714a6f07bb37bf39b14a121/work/1c/a6f55554688efec03a9177f95705c4/.command.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ue
-set -x
-cat batch_1.csv | head -n 1 > submission_report.csv
-for f in batch_1.csv; do
-    tail -n +2 "$f" >> submission_report.csv
-done
--- .nf-test/tests/75946f024946f2daa29a50048bd644e2/work/bb/7b24c737208014d3f27c481e207ed4/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-repeatmasker_liftoff.py         --repeatm_gff NY0006.fasta.out.gff         --liftoff_gff NY0006.liftoff-orig.gff         --refgff ref.MPXV.NC063383.v7.gff         --fasta NY0006.fasta          --sample_name NY0006
